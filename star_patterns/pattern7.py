63becebd750a154d4a77259e4627ff9488b440dd
unknown <ch.hemanth0508.@gmail.com>
2026-06-18 21:09:34 +0530

    updated pattern7

--- star_patterns/pattern7.py
+++ star_patterns/pattern7.py
@@ -1,1 +1,14 @@
 
+n=int(input())
+for i in range(1,n+1):
+    for sp in range(n+1,i,-1):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+for i in range(1,n+1):
+    for sp in range(1,i+1):
+        print(" ",end="")
+    for j in range(n+1,i,-1):
+        print("*",end=" ")
+    print()
